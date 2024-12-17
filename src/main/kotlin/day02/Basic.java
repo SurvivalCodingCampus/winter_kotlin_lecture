c750641028f97c8ea61eccb493398e68aecb79ab
JaeYoun Kim <rlawodusx@naver.com>
2024-12-17 18:35:15 +0900

    docs: Git 브랜치 전략 및 Kotlin 소개 정리

---
+++ src/main/kotlin/day02/Basic.java
@@ -0,0 +1,13 @@
+package day02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Basic {
+    public static void main(String[] args) {
+        List<String> items = new ArrayList<String>();
+        items.add("apple");
+        items.add("banana");
+        System.out.println(items.size());
+    }
+}
---
+++ src/main/kotlin/day02/Basic.kt
@@ -0,0 +1,26 @@
+package day02
+
+
+fun main() {
+    println("Hello World")
+
+
+    // 상수
+    val number = 10f
+
+    // 변수
+    var name = "홍길동"
+    name = "한석봉"
+
+    val items = listOf("banana", "apple", "kiwi")
+
+    for (item in items) {
+        println(item)
+    }
+
+    println(items)
+}
+
+fun sum(a: Int, b: Int) = a + b
+
+
