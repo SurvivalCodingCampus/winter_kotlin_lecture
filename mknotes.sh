c8e5009419dc4ad337a3c3666d5c71d16dc3c5ac
Serim Son <serim711@gmail.com>
2024-12-17 14:22:25 +0900

    tooling: add symlink too

--- mknotes.sh
+++ mknotes.sh
@@ -4,5 +4,6 @@ daily_md="./docs/daily/$title.md"
 
 [ -d ./docs/daily/ ] || mkdir -p ./docs/daily/
 cat <(printf '# %s\n' "$title") ./til_template.md >> "$daily_md"
+ln -sf "$daily_md" today.md
 
 printf "\n- [%s](%s): <insert description>" "$title" "$daily_md" >> "TOC.md"
