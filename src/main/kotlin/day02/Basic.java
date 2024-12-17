c0a8d5996886f8eee85c9d36ff9bc24a9cae25fe
Junsuk Oh <junsuk.oh@survivalcoding.com>
2024-12-17 15:54:40 +0900

    Kotlin 기본 문법

---
+++ src/main/kotlin/day02/Basic.java
@@ -0,0 +1,17 @@
+package day02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Basic {
+    public static void main(String[] args) {
+        String name = null;
+
+        System.out.println(name.toUpperCase());
+
+        List<String> items = new ArrayList<>();
+        items.add("apple");
+        items.add("banana");
+
+    }
+}
---
+++ src/main/kotlin/day02/Basic.kt
@@ -0,0 +1,47 @@
+package com.survivalcoding.day02
+
+fun main() {
+    println("Hello World")
+
+    // 상수
+    val number = 10     // Int
+
+    // 변수
+    var name = "길동"    // String
+    name = "석봉"
+
+    println(plus5(10))
+
+    val fullName = "홍$name"
+
+    val a = 10
+    val b = 5
+    val c = if (a > b) a else b
+
+    // 수정 안 되는 리스트
+    val items : List<String> = listOf("apple", "banana")
+    println(items)
+
+    for (item in items) {
+        println(item)
+    }
+
+    // Null Safety
+    val name2: String? = null
+    val name3: String = ""
+
+    println(name2?.uppercase())
+    println(name3.uppercase())
+}
+
+// f(x) = x + 5
+// f(5) = 25
+// f(3) = ?
+
+fun plus5(x: Int): Int = x + 5
+
+fun printSum(a: Int, b: Int) {
+    println(a + b)
+}
+
+// Java: void printSum(int a, int b) { }
