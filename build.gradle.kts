e3aee700a029741b0ef48190a3485df76d9cbb97
junho1124 <wnsghqwe@gmail.com>
2024-12-18 19:50:39 +0900

    feat: kotest를 이용한 test code 작성

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+val kotestVersion = "5.5.4"
 
 repositories {
     mavenCentral()
@@ -12,6 +13,9 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     testImplementation("junit:junit:4.13.1")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
 }
 
 tasks.test {
