24b7ae3633b494aa30e9ac0bb018a58e7034722f
Tom <yjl410720@naver.com>
2024-12-18 16:22:50 +0900

    연습문제1 제출

---
+++ src/main/kotlin/day03/Solution.kt
@@ -0,0 +1,11 @@
+package org.example.day03
+
+class Cleric(
+    val name: String = "",  // 이름을 초기 설정 후 변경이 불가.
+    var hp: Int = 50,
+    var mp: Int = 10,
+) {
+    val MAX_HP = 50
+    val MAX_MP = 10
+
+}
