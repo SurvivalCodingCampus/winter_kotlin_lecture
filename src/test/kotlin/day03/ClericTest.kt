713d921e1e31174a57fb94317b0cf04e29489639
Tom <yjl410720@naver.com>
2024-12-18 18:44:50 +0900

    2024-12-18 TIL 제출

---
+++ src/test/kotlin/day03/ClericTest.kt
@@ -0,0 +1,17 @@
+package day03
+
+import org.junit.Assert.*
+ class ClericTest {
+
+@org.junit.Before
+ fun setUp() {}
+
+@org.junit.After
+ fun tearDown() {}
+
+@org.junit.Test
+ fun selfAid() {}
+
+@org.junit.Test
+ fun pray() {}
+}
