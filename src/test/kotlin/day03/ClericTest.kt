96c8989d3dae63f401c198e06172e1def4ed6c44
Yujin Choe <dev.yujinchoe@gmail.com>
2024-12-18 18:16:47 +0900

    docs: 241218 클래스인스턴스 테스트코드  실습

---
+++ src/test/kotlin/day03/ClericTest.kt
@@ -0,0 +1,21 @@
+package day03
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+ class ClericTest {
+
+@Before
+ fun setUp() {}
+
+@After
+ fun tearDown() {}
+
+@Test
+ fun selfAid() {}
+
+@Test
+ fun pray() {}
+}
