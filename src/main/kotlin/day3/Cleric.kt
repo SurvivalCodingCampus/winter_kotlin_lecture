0be943d7b1fda9beaf6ccc4aa3d77f0a3ca59a1d
SibangGyeon <briankim0695@gmail.com>
2024-12-19 18:23:25 +0900

    연습문제 2-A

--- src/main/kotlin/day3/Cleric.kt
+++ src/main/kotlin/day3/Cleric.kt
@@ -1,27 +1,35 @@
 package org.example.day3
 
+//const val MAX_HP = 50
+//const val MAX_MP = 10
+
 class Cleric(
-    val name: String,
-    var hp: Int = 50,
-    val maxHp: Int = 200,
-    var mp: Int = 25,
-    val maxMp: Int = 100
+    val name: String = "사제",
+    var hp: Int = MAX_HP,
+    var mp: Int = MAX_MP
 ) {
+    companion object {
+        const val MAX_HP = 50
+        const val MAX_MP = 10
+    }
+
+
     fun selfAid() {
         if (mp >= 5) {
             mp -= 5
-            hp = maxHp
+            hp = MAX_HP
         }
     }
 
     fun pray(prayTime: Int): Int {
-//        val numberRange = (0..2)
         val number = (0..2).random()
         val startMp = mp
 
+        if ((startMp == MAX_MP) || (prayTime <= 0)) return 0
+
         mp += (prayTime + number)
 
-        if (mp > maxMp) mp = maxMp
+        if (mp > MAX_MP) mp = MAX_MP
 
         val recoveryMp = mp - startMp
 
--- src/test/kotlin/day3/ClericTest.kt
+++ src/test/kotlin/day3/ClericTest.kt
@@ -4,31 +4,91 @@ import org.example.day3.Cleric
 import org.junit.After
 import org.junit.Before
 import org.junit.Assert.*
+import org.junit.Test
 
 class ClericTest {
 
- @Before
- fun setUp() {}
+    @Before
+    fun setUp() {
+    }
 
- @After
- fun tearDown() {}
+    @After
+    fun tearDown() {
+    }
 
- @org.junit.Test
- fun selfAId() {
-  val cleric = Cleric(name = "사제")
+    @Test
+    fun `피격 후 회복 과정을 마나 고갈시까지 반복`() {
+        val cleric = Cleric(name = "사제")
 
-  for (i in 1..5) {
-   cleric.selfAid()
-   cleric.hp -= 100
-  }
+        while (true) {
+            cleric.hp -= 5
+            if (cleric.mp < 5) break
+            cleric.selfAid()
+        }
 
-  assertEquals(100, cleric.hp)
- }
+        assertEquals(45, cleric.hp)
+    }
 
- @org.junit.Test
- fun pray() {
-  val cleric = Cleric(name = "사제")
+    @Test
+    fun `MP가 부족할 때 selfAid는 동작하지 않음`() {
+        val cleric = Cleric(name = "사제")
+        cleric.mp = 4
+        val initialHp = cleric.hp
 
-  assertTrue(cleric.pray(3) in 3..5)
- }
+        cleric.selfAid()
+
+        assertEquals(initialHp, cleric.hp)
+        assertEquals(4, cleric.mp)
+    }
+
+    @Test
+    fun `pray - mp가 max 가 아닐 때 mp 를 회복하는 경우(time + 0~2)`() {
+        val cleric = Cleric(name = "사제")
+        cleric.mp = (0..<Cleric.MAX_MP).random()
+        assertTrue(cleric.pray(3) in 3..5)
+    }
+
+    @Test
+    fun `pray - mp 가 max 인 경우에는 회복 안 됨`() {
+        val cleric = Cleric("홍길동")
+
+        assertEquals(0, cleric.pray(1))
+        assertEquals(0, cleric.pray(2))
+        assertEquals(0, cleric.pray(3))
+    }
+
+    @Test
+    fun `pray - mp 가 time 이 0 이하이면 회복 안 됨`() {
+        val cleric = Cleric("홍길동")
+        cleric.mp = 0
+
+        assertEquals(false, cleric.pray(1) == 0)
+        assertEquals(0, cleric.pray(0))
+        assertEquals(0, cleric.pray(-1))
+    }
+
+    @Test
+    fun `연습문제 2-A 인스턴스화`() {
+        // 이 클래스는 Cleric(“아서스", hp = 40, mp = 5) 와 같이,
+        // 이름, HP, MP 를 지정하여 인스턴스화 할 수 있다
+        val cleric1 = Cleric("아서스", hp = 40, mp = 5)
+
+        assertEquals("아서스", cleric1.name)
+        assertEquals(40, cleric1.hp)
+        assertEquals(5, cleric1.mp)
+
+        // 다른 경우 Cleric(“리치왕", hp = 45, mp = 10)
+        val cleric2 = Cleric("리치왕", hp = 45, mp = 10)
+
+        assertEquals("리치왕", cleric2.name)
+        assertEquals(45, cleric2.hp)
+        assertEquals(10, cleric2.mp)
+
+//        // hp와 mp가 max 보다 큰 경우 Cleric(“리치왕", hp = 55, mp = 15)
+//        val cleric3 = Cleric("리치왕", hp = 55, mp = 15)
+//
+//        assertEquals(Cleric.MAX_HP, cleric3.hp)
+//        assertEquals(Cleric.MAX_MP, cleric3.mp)
+    }
+
 }
