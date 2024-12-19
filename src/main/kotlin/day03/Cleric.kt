2544cbf63cd90e6f6719d0894c6f0f499780da0f
Tom <yjl410720@naver.com>
2024-12-19 18:41:25 +0900

    update source codes

--- src/main/kotlin/day03/Cleric.kt
+++ src/main/kotlin/day03/Cleric.kt
@@ -1,25 +1,29 @@
 package org.example.day03
 
+
 class Cleric(
     val name: String,  // 이름을 초기 설정 후 변경이 불가.
-    var hp: Int = 50,
-    var mp: Int = 10,
+    var hp: Int = MAX_HP,
+    var mp: Int = MAX_MP,
 ) {
-    val MAX_HP = 50
-    val MAX_MP = 10
+    companion object {
+        const val MAX_HP = 50
+        const val MAX_MP = 10
+    }
 
     fun selfAid(): Unit {
         if (mp < 5) {
+            println("mp가 부족합니다.")
             return
         }
 
         mp -= 5
         hp = MAX_HP
-
     }
 
     fun pray(sec: Int): Int {
         if (sec <= 0) {
+            println("0초 이하로 pray 할 수는 없습니다.")
             return 0
         }
 
