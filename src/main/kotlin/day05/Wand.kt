985f1bc217384251c60051d2ab33ee584f2b077d
Yujin Choe <dev.yujinchoe@gmail.com>
2024-12-22 03:39:22 +0900

    연습문제1-2.Wand Wizard 생성시 타당성 규칙 검사 추가

--- src/main/kotlin/day05/Wand.kt
+++ src/main/kotlin/day05/Wand.kt
@@ -1,3 +1,17 @@
 package day05
 
-class Wand(var name: String, var power: Double)
+/*
+지팡이의 이름은 null 일 수 없고, 반드시 3문자 이상이어야 한다
+지팡이의 마력은 0.5 이상 100.0 이하여야 한다.
+*/
+class Wand(name: String, power: Double) {
+    var name: String = name
+        private set
+
+    var power: Double = power
+
+    init {
+        require(name.length >= 3) { "지팡이는 반드시 3문자 이상이어야 합니다" }
+        require(power >= 0.5 && power <= 100) { "지팡이 마력은 0.5 이상 100.0 이하여야 합니다" }
+    }
+}
