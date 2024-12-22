516c2bccf03d52da2ce7babb166879e5c14178b4
최현정 <hjchoi.dev@gmail.com>
2024-12-22 23:37:02 +0900

    fix: assertThrows 제거

--- src/test/kotlin/day04/ClericTest.kt
+++ src/test/kotlin/day04/ClericTest.kt
@@ -2,7 +2,6 @@ package day04
 
 import org.hyunjung.day04.Cleric
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertThrows
 import org.junit.Test
 
 class ClericTest {
@@ -38,27 +37,18 @@ class ClericTest {
 
     // D) 이름을 지정하지 않는 경우 - 컴파일 에러로 인해 테스트할 필요 없음
 
-    @Test
+    @Test(expected = IllegalArgumentException::class)
     fun `이름이 빈 문자열이면 예외 발생`() {
-        val exception = assertThrows(IllegalArgumentException::class.java) {
-            Cleric(name = "")
-        }
-        assertEquals("이름은 비어 있을 수 없습니다.", exception.message)
+        Cleric(name = "")
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException::class)
     fun `HP가 유효 범위를 벗어나면 예외 발생`() {
-        val exception = assertThrows(IllegalArgumentException::class.java) {
-            Cleric(name = "아서스", hp = 60)
-        }
-        assertEquals("HP는 0 이상 50 이하여야 합니다.", exception.message)
+        Cleric(name = "아서스", hp = 60)
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException::class)
     fun `MP가 유효 범위를 벗어나면 예외 발생`() {
-        val exception = assertThrows(IllegalArgumentException::class.java) {
-            Cleric(name = "아서스", hp = 40, mp = -1)
-        }
-        assertEquals("MP는 0 이상 10 이하여야 합니다.", exception.message)
+        Cleric(name = "아서스", hp = 40, mp = -1)
     }
 }
