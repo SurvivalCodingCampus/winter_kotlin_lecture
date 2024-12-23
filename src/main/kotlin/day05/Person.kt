6180ce17770361c9833686ee77b0d1608b920b65
Junsuk Oh <junsuk.oh@survivalcoding.com>
2024-12-23 15:26:40 +0900

    day06 - 프로퍼티 연습문제

---
+++ src/main/kotlin/day05/Person.kt
@@ -0,0 +1,12 @@
+package com.survivalcoding.day05
+
+import java.time.LocalDate
+
+class Person2(
+    val name: String,
+    val birthYear: Int,
+    val localDate: LocalDate = LocalDate.now()
+) {
+    val age: Int
+        get() = localDate.year - birthYear
+}
