7857c7a19b179ac8f3c0a60ce477d3394996376b
Serim Son <serim711@gmail.com>
2024-12-23 17:09:38 +0900

    feat: implement slime basic attack

--- src/main/kotlin/day3/Slime.kt
+++ src/main/kotlin/day3/Slime.kt
@@ -7,4 +7,11 @@ open class Slime(
     suffix: String,
     hp: Int = 50,
 ) : Actor(name = "Slime $suffix", HealthPoint(hp)) {
+
+    var target: Actor? = null
+    open fun attack() {
+        if (target == null) return
+        println("Basic attack to $target")
+    }
+
 }
