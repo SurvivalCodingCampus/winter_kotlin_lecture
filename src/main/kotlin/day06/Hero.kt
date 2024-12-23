aefc9ced128e44e699bd1be707882812068ce1a4
JaeYoun Kim <rlawodusx@naver.com>
2024-12-24 02:11:36 +0900

    Refactor: Hero Code Refactoring

--- src/main/kotlin/day06/Hero.kt
+++ src/main/kotlin/day06/Hero.kt
@@ -11,10 +11,12 @@ class Hero(
 
     // 호출되면 영웅이 데미지를 받는 코드
     fun getDamage(damage: Int) {
-        if (hp > 0) {
-            hp = (hp - damage).coerceAtLeast(0)     // 체력이 0 아래로 떨어질경우 0으로 처리
-            println("${damage}포인트의 데미지")
-        }
+
+        if (damage < 0) return      // 데미지가 음수일 수 없음
+
+        hp = (hp - damage).coerceAtLeast(0)     // 체력이 0 아래로 떨어질경우 0으로 처리
+        println("${damage}포인트의 데미지")
+
         // hp가 0일 경우 사망 처리
         if (hp == 0) die()
     }
@@ -22,10 +24,13 @@ class Hero(
 
     // 마법사로부터 힐을 받으면 호출
     fun getHeal(healAmount: Int) {
-        if (hp + healAmount > MAX_HP) {
-            hp = MAX_HP
+        // healAmount가 음수거나 히어로가 이미 사망한 경우
+        if (healAmount < 0 || isDead) return
+
+        hp = if (hp + healAmount > MAX_HP) {
+            MAX_HP
         } else {
-            hp += healAmount
+            hp + healAmount
         }
 
     }
