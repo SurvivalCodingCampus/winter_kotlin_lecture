3331d9a96a2dd873987869872c290c5ddadb7b11
junho1124 <wnsghqwe@gmail.com>
2024-12-24 16:11:49 +0900

    feat: 연습문제 1
    - Book, Computer class 추가

---
+++ src/main/kotlin/day07/Book.kt
@@ -0,0 +1,9 @@
+package org.example.day07
+
+class Book(
+    var name: String,
+    var price: Int,
+    var color: String,
+    var isbn: String,
+) {
+}
---
+++ src/main/kotlin/day07/Computer.kt
@@ -0,0 +1,9 @@
+package org.example.day07
+
+class Computer(
+    var name: String,
+    var price: Int,
+    var color: String,
+    var makerName: String,
+) {
+}
