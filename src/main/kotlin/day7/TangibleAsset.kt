d2bc095c063398bcae002c3d78355ad1cc5646a8
Serim Son <serim711@gmail.com>
2024-12-24 19:36:33 +0900

    refactor: prevent weight from being overriden

--- src/main/kotlin/day7/TangibleAsset.kt
+++ src/main/kotlin/day7/TangibleAsset.kt
@@ -4,5 +4,5 @@ abstract class TangibleAsset(
     name: String,
     var price: Int,
     var color: String,
-    override var weight: Double,
+    final override var weight: Double,
 ) : Asset(name), Thing
