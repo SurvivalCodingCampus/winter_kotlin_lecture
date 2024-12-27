adc6a55622d8d8515c847eb3960cedee93fa8082
hodak <jlkim909@gmail.com>
2024-12-27 20:09:29 +0900

    feat: deep copy가 가능한 Book 클래스 구현
    equals/hashCode 함수 재정의
    sorted() 수행 가능
    deep copy 제공

---
+++ src/main/kotlin/day09/Book.kt
@@ -0,0 +1,45 @@
+package org.example.day09
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class Book(
+    val title: String,
+    val author: String,
+    val publishedDate: LocalDateTime = LocalDateTime.now()
+) : Comparable<Book> {
+    private val formattedPublishedDate =
+        publishedDate.format(DateTimeFormatter.ISO_DATE)
+
+    override fun compareTo(other: Book): Int {
+        return formattedPublishedDate.compareTo(other.formattedPublishedDate)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Book
+
+        if (title != other.title) return false
+        if (formattedPublishedDate != other.formattedPublishedDate) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = title.hashCode()
+        result = 31 * result + author.hashCode()
+        result = 31 * result + formattedPublishedDate.hashCode()
+        return result
+    }
+
+    fun deepCopy(
+        title: String = this.title,
+        author: String = this.author,
+        publishedDate: LocalDateTime = this.publishedDate
+    ): Book {
+        val newLocalDateTime = LocalDateTime.of(publishedDate.toLocalDate(), publishedDate.toLocalTime())
+        return Book(title, author, newLocalDateTime)
+    }
+}
