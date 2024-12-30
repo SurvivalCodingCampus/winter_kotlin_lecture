bc3d8b0abd02c6e790b848e17dfb77539fae0aee
junho1124 <wnsghqwe@gmail.com>
2024-12-30 16:31:23 +0900

    feat: 연습문제 2
    - KeyType.kt 추가
    - StrongBox.kt 수정

--- src/main/kotlin/day10/StrongBox.kt
+++ src/main/kotlin/day10/StrongBox.kt
@@ -1,11 +1,19 @@
 package org.example.day10
 
-class StrongBox<E> {
+
+class StrongBox<E>(private val key: KeyType) {
     private var _item: E? = null
+    private var _trialCount: Int = 0
 
     fun put(item: E) {
         _item = item
     }
 
-    fun get(): E? = _item
+    fun get(): E? {
+        if (_trialCount < key.trialLimit) {
+            _trialCount++
+            return null
+        }
+        return _item
+    }
 }
