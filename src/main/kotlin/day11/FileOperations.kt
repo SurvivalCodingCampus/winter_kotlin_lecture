1620c38025994ad7f30b1464862913afafb053fa
Serim Son <serim711@gmail.com>
2024-12-31 18:18:13 +0900

    docs: daily dev notes

--- src/main/kotlin/day11/FileOperations.kt
+++ src/main/kotlin/day11/FileOperations.kt
@@ -1,6 +1,8 @@
 package day11
 
+import java.io.File
 import java.io.FileNotFoundException
+import java.io.OutputStream
 import kotlin.io.path.Path
 
 interface FileOperations {
@@ -24,4 +26,18 @@ class DefaultFileOperations : FileOperations {
     }
 
     private fun pathResolver(fromPath: String, toPath: String) = Path(fromPath) to Path(toPath)
+
+    fun copyOverStream(fileToCopy: File, destination: OutputStream) {
+        val fileInputStream = fileToCopy.inputStream()
+        val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
+        var bytesRead = fileInputStream.read(buffer)
+        while (bytesRead != -1) { // EOF Check
+            destination.write(buffer, 0, bytesRead)
+            destination.flush()
+            bytesRead = fileInputStream.read(buffer)
+        }
+
+        fileInputStream.close()
+        destination.close()
+    }
 }
