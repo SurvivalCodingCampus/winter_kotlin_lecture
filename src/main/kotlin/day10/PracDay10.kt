095f2cf5143031b445f69d0856101cd696048f2f
steve <diasm2@gmail.com>
2024-12-31 13:13:56 +0900

    fix : condition in get method

--- src/main/kotlin/day10/PracDay10.kt
+++ src/main/kotlin/day10/PracDay10.kt
@@ -6,13 +6,17 @@ enum class KeyPad {
 }
 
 
-class KeyPadLimitation {
+class LimitOfPad {
     companion object {
         const val PAD_LOCK = 1024
         const val BUTTON = 10000
         const val DIAL = 30000
         const val FINGER = 1000000
     }
+}
+
+enum class Hello {
+    WORLD, SEOUL, BUSAN, HEHE
 }
 
 
@@ -28,7 +32,7 @@ class StrongBox<T>(keyType: T) {
     fun get(): T? {
         _counter += 1
 
-        if (_counter > checkingLimit(_keyPadType)) {
+        if (_counter >= checkingLimit(_keyPadType)) {
             println("${_keyPadType}이 해제되었습니다.${_counter}")
         } else {
             return null
@@ -40,10 +44,10 @@ class StrongBox<T>(keyType: T) {
 
     private fun checkingLimit(keyType: T?): Int {
         return when (keyType) {
-            KeyPad.PAD_LOCK -> KeyPadLimitation.PAD_LOCK
-            KeyPad.BUTTON -> KeyPadLimitation.BUTTON
-            KeyPad.DIAL -> KeyPadLimitation.DIAL
-            KeyPad.FINGER -> KeyPadLimitation.FINGER
+            KeyPad.PAD_LOCK -> LimitOfPad.PAD_LOCK
+            KeyPad.BUTTON -> LimitOfPad.BUTTON
+            KeyPad.DIAL -> LimitOfPad.DIAL
+            KeyPad.FINGER -> LimitOfPad.FINGER
             else -> {
                 0
             }
@@ -57,6 +61,7 @@ class StrongBox<T>(keyType: T) {
 fun main() {
 
     val instance2 = StrongBox(KeyPad.DIAL)
+
 
     repeat(30001) {
         instance2.get()
