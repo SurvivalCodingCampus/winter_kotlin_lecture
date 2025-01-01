d445c02d3363447581e04dbb3bf842b720c1bff7
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-01 21:52:49 +0900

    feat: 제네릭,열거형 연습문제 StrongBox클래스 및 테스트코드

--- src/test/kotlin/day10/StrongBoxTest.kt
+++ src/test/kotlin/day10/StrongBoxTest.kt
@@ -1,4 +1,26 @@
 package day10
 
-import org.junit.jupiter.api.Assertions.*
- class StrongBoxTest
+import java.security.Key
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+
+class StrongBoxTest {
+    @Test
+    fun `각 KeyType별로 최대 시도횟수 도달 전 null반환 및 도달 후 내용물 get 확인`() {
+        KeyType.entries.forEach { keyType ->
+            val box = StrongBox<String>(keyType)
+
+            box.put("goldBar")
+
+            repeat(keyType.maxAttempts) {
+                assertNull(box.get())
+            }
+
+            assertEquals("goldBar", box.get())
+        }
+    }
+
+}
