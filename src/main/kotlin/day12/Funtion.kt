68bba919ab5c8db2ef3f763c6671c00240cb6c9d
SibangGyeon <briankim0695@gmail.com>
2025-01-02 20:27:02 +0900

    12일차 디버깅, 람다식과 함수 수업

---
+++ src/main/kotlin/day12/Funtion.kt
@@ -0,0 +1,41 @@
+package org.example.day12
+
+fun main() {
+    listOf(3).reduce { acc, i -> i }
+
+//    command({ damage -> println() })
+
+    // 무명클래스
+    Timer().start {
+        println("땡!")
+    }
+}
+
+//interface OnCompleteCallback {
+//    // input도 없고 output도 없는 함수
+//    fun onComplete(): Unit
+//}
+
+class Timer {
+    fun start(callback: () -> Unit) {
+        println("타이머 시작")
+        Thread.sleep(3000)
+        callback()
+    }
+}
+
+
+class MyComparator : Comparator<Int> {
+    override fun compare(o1: Int?, o2: Int?): Int {
+        return o1!! - o2!!
+    }
+}
+
+fun command(func: Attackable) {
+
+}
+
+// SAM
+interface Attackable {
+    fun attack(damage: Int)
+}
