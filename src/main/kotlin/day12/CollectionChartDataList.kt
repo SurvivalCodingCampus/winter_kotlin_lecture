3651aa3553a380a6ba6d87ec2b288c8fd944ae61
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-03 02:08:42 +0900

    feat: JSON 역직렬화시 데이터 구분 pk 필드 추가

--- src/main/kotlin/day12/CollectionChartDataList.kt
+++ src/main/kotlin/day12/CollectionChartDataList.kt
@@ -1,9 +1,13 @@
 package day12
 
+import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 /*
 다음과 같은 데이터를 서버에서 받아서 처리해야 한다. 문제점이 있다면 생각해 보고 해결 방안을 고민해 보시오.
@@ -13,8 +17,10 @@ LocalDateTime을 사용하여 DateTimeFormatter를 사용하여 문자열을 Loc
 2. null 데이터 처리
 List<CollectionSalePrice>? nullable 타입 사용하여 null 값 처리.
 
-3. price가 Double 형식이 아닌 경우 예외 처리
-price 값을 Double로 변환 실패 시 예외를 던져 처리
+3. 데이터 구분
+받아오는 JSON 데이터 형식은 변경할 수 없고, 데이터 클래스만 변경해야 한다면,
+JSON 역직렬화 후에 pk를 추가하여 구분할 수 있도록 처리 필요
+
 */
 
 @Serializable
@@ -26,26 +32,25 @@ data class CollectionChartDataList(
 data class CollectionChartData(
     val collectionName: String,
     val collectionSalePrice: List<CollectionSalePrice>?,
+    @Transient var pk: Int = -1 // 직렬화시 제외, 초기값 -1로, var로 수정 가능하도록
 )
 
 @Serializable
 data class CollectionSalePrice(
     val price: Double,
-    val cvtDatetime: String,
+    val cvtDatetime: String
 )
 
 fun main() {
-    val collection1 = CollectionSalePrice(59.75, "2023-03-26T08:08:35")
-    val collection2 = CollectionSalePrice(60.00, "2023-03-26T08:08:45")
-    val collectionChartData1 = CollectionChartData("collectionName", null)
-    val collectionChartData2 = CollectionChartData("collectionName", listOf(collection1, collection2))
-    val collectionChartDataList = CollectionChartDataList(listOf(collectionChartData1, collectionChartData2))
-    val json = Json.encodeToString(collectionChartDataList)
-    println(json)
-
     // 역직렬화
     val objString = File("JsonExam.txt").readText()
     val obj = Json.decodeFromString<CollectionChartDataList>(objString)
+    println(obj)
+
+    // 역직렬화 후, PK 값 추가 (1부터 순차적으로 pk 값 부여)
+    obj.collectionChartDataList.forEachIndexed { index, collectionChartData ->
+        collectionChartData.pk = index + 1 // 1부터 시작하도록 pk 부여
+    }
     println(obj)
 
     // obj 직렬화
