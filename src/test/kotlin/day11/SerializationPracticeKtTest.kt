ff31681583ee8891866e2a1c0ba5b5b68d5c57dd
SibangGyeon <briankim0695@gmail.com>
2025-01-02 11:47:49 +0900

    직렬화 테스트 코드 추가, 수정

--- src/test/kotlin/day11/SerializationPracticeKtTest.kt
+++ src/test/kotlin/day11/SerializationPracticeKtTest.kt
@@ -1,5 +1,6 @@
 package day11
 
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.example.day11.Department
@@ -24,8 +25,15 @@ class SerializationPracticeKtTest {
     @Test
     fun `인스턴스 생성하고 JSON으로 직렬화하여 txt 파일에 저장`() {
         val file = File("company.txt")
+        val staff = Department("총무부", Employee("홍길동", 41))
+        val json = Json.encodeToString(staff)
 
-        file.writeText(Json.encodeToString(Department("총무부", Employee("홍길동", 41))))
+        file.writeText(json)
+
+        val checkText = Json.decodeFromString<Department>(file.readText()).toString()
+
+        assertEquals("{\"name\":\"총무부\",\"leader\":{\"name\":\"홍길동\",\"age\":41}}", json)
+        assertEquals("Department(name=총무부, leader=Employee(name=홍길동, age=41))", checkText)
 
         file.delete()
     }
