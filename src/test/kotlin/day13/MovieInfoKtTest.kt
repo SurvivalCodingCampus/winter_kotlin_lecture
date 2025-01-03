b7722619f1dbf5ea5732a120e6917bd554704a7e
SibangGyeon <briankim0695@gmail.com>
2025-01-03 20:00:12 +0900

    day13: 비동기 연습문제 1

--- src/test/kotlin/day13/MovieInfoKtTest.kt
+++ src/test/kotlin/day13/MovieInfoKtTest.kt
@@ -9,6 +9,7 @@ import org.junit.Assert.*
 import org.junit.Test
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
+import kotlin.system.measureNanoTime
 
 class MovieInfoKtTest {
 
@@ -21,12 +22,16 @@ class MovieInfoKtTest {
     }
 
     @Test
-    fun `director가 출력되는지 테스트`(): Unit = runBlocking {
+    fun `director가 출력(1초 딜레이)되는지 테스트`(): Unit = runBlocking {
         var outputStream = ByteArrayOutputStream()
         System.setOut(PrintStream(outputStream))
 
-        println(getMovieInfo().director)
+        val measuredTime = measureNanoTime {
+            println(getMovieInfo().director)
+        }
+
         assertEquals("George Lucas", outputStream.toString().trim())
+        assertEquals(1, measuredTime / 1000000000)
 
         System.setOut(System.out)
     }
