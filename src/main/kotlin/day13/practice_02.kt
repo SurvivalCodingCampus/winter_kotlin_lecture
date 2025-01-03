79bc0882d16f2653c2882ec20b3f3fd8e2f9b5cf
execution720 <JustTomGo@gmail.com>
2025-01-03 16:04:43 +0900

    checkPoint to practice_02

---
+++ src/main/kotlin/day13/practice_02.kt
@@ -0,0 +1,40 @@
+package day13
+
+import kotlinx.coroutines.*
+
+class FirstBird {
+    suspend fun BirdSound() {
+        delay(1000)
+        println("꾸우")
+    }
+}
+
+class SecondBird {
+    suspend fun BirdSound() {
+        delay(2000)
+        println("까악")
+    }
+}
+
+class ThirdBird {
+    suspend fun BirdSound() {
+        delay(3000)
+        println("짹짹")
+    }
+}
+
+fun main(): Unit = runBlocking {
+    val bird1 = FirstBird()
+    val bird2 = SecondBird()
+    val bird3 = ThirdBird()
+
+    launch {
+        repeat(4) {
+            bird1.BirdSound()
+            bird2.BirdSound()
+            bird3.BirdSound()
+        }
+    }
+
+}
+
