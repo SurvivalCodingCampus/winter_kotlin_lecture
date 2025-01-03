ad6ef3abadfbb977de20dd9eeb05c587d8249a5a
junho1124 <wnsghqwe@gmail.com>
2025-01-03 16:00:59 +0900

    test: 새가 singShort 메서드를 4번 실행하도록 수정 (student/07-최준호)

--- src/main/kotlin/day13/Bird.kt
+++ src/main/kotlin/day13/Bird.kt
@@ -3,14 +3,11 @@ package org.example.day13
 import kotlinx.coroutines.delay
 
 class Bird(val sound: String, val soundDelay: Long) {
-    var count = 0
-        private set
 
-    suspend fun singFourTimes() {
-        repeat(4) {
+    suspend fun singShort() {
+        repeat(SHORT_SING_COUNT) {
             println(sound)
             delay(soundDelay)
-            count++
         }
     }
 }
--- src/test/kotlin/day13/BirdTest.kt
+++ src/test/kotlin/day13/BirdTest.kt
@@ -1,6 +1,8 @@
 package day13
 
 import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.example.day13.Bird
 import org.junit.Assert.assertTrue
@@ -8,20 +10,20 @@ import org.junit.Test
 import kotlin.system.measureTimeMillis
 
 class BirdTest {
+    val 꾸우Bird = Bird("꾸우", 1000L)
+    val 까악Bird = Bird("까악", 2000L)
+    val 짹짹Bird = Bird("짹짹", 3000L)
     @Test
     fun `sing 4 times`() = runBlocking {
-        val 꾸우Bird = Bird("꾸우", 1000L)
-        val 까악Bird = Bird("까악", 2000L)
-        val 짹짹Bird = Bird("짹짹", 3000L)
 
         val result1 = async {
-            꾸우Bird.singFourTimes()
+            꾸우Bird.singShort()
         }
         val result2 = async {
-            까악Bird.singFourTimes()
+            까악Bird.singShort()
         }
         val result3 = async {
-            짹짹Bird.singFourTimes()
+            짹짹Bird.singShort()
         }
 
         val time = measureTimeMillis {
@@ -30,6 +32,10 @@ class BirdTest {
             result3.await()
         }
         println("시간: $time")
-        assertTrue("시간이 12초 이상 걸림", time > 12000)
+        assertTrue(
+            "시간이 ${짹짹Bird.soundDelay * Bird.SHORT_SING_COUNT}초 이상 걸림",
+            time > 짹짹Bird.soundDelay * Bird.SHORT_SING_COUNT
+        )
+        println("bird1: ${result1.isActive}, bird2: ${result2.isActive}, bird3: ${result3.isActive}")
     }
 }
