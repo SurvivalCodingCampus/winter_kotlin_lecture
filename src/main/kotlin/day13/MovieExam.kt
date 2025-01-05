d3887f7088c3cf50d758db894be17da0c5e59a83
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-05 16:03:14 +0900

    feat: 연습문제1. 영화 정보 받아오는 suspend 함수

---
+++ src/main/kotlin/day13/MovieExam.kt
@@ -0,0 +1,45 @@
+package day13
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+/*
+연습문제1. API호출 및 데이터 처리
+다음과 같은 영화 정보를 반환하는 함수가 있다.
+Movie 클래스를 작성,
+위 함수를 완성하고 director가 누군지 출력하는 main 함수를 작성하시오.
+*/
+
+@Serializable
+data class Movie(
+    val title: String,
+    val director: String,
+    val year: Int
+)
+
+class MovieExam {
+    suspend fun getMovieInfo(): Movie {
+        delay(1000)
+        val json = """
+            {
+            "title": "Star Wars", 
+            "director": "George Lucas",
+            "year": 1977
+            }
+        """.trimIndent()
+        // TODO : Movie 데이터를 리턴하도록 작성
+        // Movie Json 파싱해서 받기
+        val movie = Json.decodeFromString<Movie>(json)
+        // TODO : TestCode. 테스트코드에서도 fun 함수명() = runBlocking{} 하면 블록 내에서 코루틴 쓸 수 있음
+
+        return movie
+    }
+}
+
+fun main() = runBlocking {
+    val movieExam = MovieExam()
+    val movie = movieExam.getMovieInfo()
+    println("Director: ${movie.director}")
+}
