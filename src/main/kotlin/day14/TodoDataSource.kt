3dfc00a2a2cea00bbfc7cfa59bffea32a87f911f
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-06 15:34:43 +0900

    feat: 연습문제2. Json List 를 List<Todo>로 변환

--- src/main/kotlin/day14/TodoDataSource.kt
+++ src/main/kotlin/day14/TodoDataSource.kt
@@ -1,12 +1,16 @@
 package day14
 
+import day12.CollectionChartDataList
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
+import java.io.File
 
 /*
 과제1. 간단한 JSON 을 데이터 클래스로 변환
+과제2. Json List 를 List<To do>로 변환
 */
+
 
 @Serializable
 data class Todo(
@@ -17,21 +21,16 @@ data class Todo(
 )
 
 interface TodoDataSource {
-    suspend fun getTodo(): Todo
+    suspend fun getTodo(): List<Todo>
 }
 
 
 class TodoDataSourceImpl : TodoDataSource {
-    private val json = """
-    {
-      "userId": 1,
-      "id": 1,
-      "title": "delectus aut autem",
-      "completed": false
-    }
-""".trimIndent()
+    val jsonString = File("JsonList.txt").readText()
+    val jsonList = Json.decodeFromString<List<Todo>>(jsonString)
 
-    override suspend fun getTodo(): Todo {
-        return Json.decodeFromString(json)
+    override suspend fun getTodo(): List<Todo> {
+        return jsonList
     }
 }
+
