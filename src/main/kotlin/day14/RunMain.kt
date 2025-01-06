819a1b6132c4891a016d6370aee54d663fad8c1b
hodak <jlkim909@gmail.com>
2025-01-07 02:29:14 +0900

    feat: main 에서 user repo 사용

--- src/main/kotlin/day14/RunMain.kt
+++ src/main/kotlin/day14/RunMain.kt
@@ -2,6 +2,7 @@ package org.example.day14
 
 import kotlinx.coroutines.runBlocking
 import org.example.day14.data.repository.TodoDataSourceImpl
+import org.example.day14.data.repository.UserDataSourceImpl
 import org.example.day14.data.result.Result
 import org.example.day14.data.result.asResult
 
@@ -36,4 +37,36 @@ fun main(): Unit = runBlocking {
     }
     println(todoRepo.getTodo())
     println(todoRepo.getTodos())
+
+    val userRepo = UserDataSourceImpl()
+
+    userRepo.getUsersFromUrl().asResult().collect { result ->
+        when (result) {
+            is Result.Success -> {
+                result.data.forEach {
+                    println(it)
+                }
+            }
+
+            is Result.Error -> throw result.exception
+            Result.Loading -> {
+                println("로딩 중..")
+            }
+        }
+    }
+
+    userRepo.getUser(1).asResult().collect { result ->
+        when (result) {
+            is Result.Success -> {
+                println(result.data)
+            }
+
+            is Result.Error -> throw result.exception
+            Result.Loading -> {
+                println("로딩 중..")
+            }
+        }
+    }
+    println(userRepo.getUser())
+    println(userRepo.getUser())
 }
