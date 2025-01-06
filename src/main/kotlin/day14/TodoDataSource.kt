90e519d22cc9916b92374a93ad5f08a237b6c7ae
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-06 14:53:25 +0900

    feat: 연습문제1.JSON을 데이터 클래스로 변환

---
+++ src/main/kotlin/day14/TodoDataSource.kt
@@ -0,0 +1,43 @@
+package day14
+
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+/*
+과제1. 간단한 JSON 을 데이터 클래스로 변환
+*/
+
+@Serializable
+data class Todo(
+    val userId: Int,
+    val id: Int,
+    val title: String,
+    val completed: Boolean
+)
+
+interface TodoDataSource {
+    suspend fun getTodo(): Todo
+}
+
+
+class TodoDataSourceImpl : TodoDataSource {
+    private val json = """
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "delectus aut autem",
+      "completed": false
+    }
+""".trimIndent()
+
+    override suspend fun getTodo(): Todo {
+        return Json.decodeFromString(json)
+    }
+}
+
+
+fun main(): Unit = runBlocking {
+    val tododata = TodoDataSourceImpl()
+    println(tododata.getTodo())
+}
