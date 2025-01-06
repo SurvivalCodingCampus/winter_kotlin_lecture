7ac4bfda2c2d4040a85eb711610f0377759df590
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-06 15:35:40 +0900

    feat: JSON이 데이터클래스 리스트로 변환되는지 테스트

--- src/test/kotlin/day14/TodoDataSourceImplTest.kt
+++ src/test/kotlin/day14/TodoDataSourceImplTest.kt
@@ -1,12 +1,11 @@
 package day14
 
 import kotlinx.coroutines.test.runTest
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import kotlin.test.*
 
 class TodoDataSourceImplTest {
 
-    @Test
+    /*@Test
     fun `JSON이 데이터클래스로 올바르게 변환되는지 확인`() = runTest {
         val todoData = TodoDataSourceImpl()
         val todo = todoData.getTodo()
@@ -15,5 +14,15 @@ class TodoDataSourceImplTest {
         assertEquals(1, todo.id)
         assertEquals("delectus aut autem", todo.title)
         assertEquals(false, todo.completed)
+    }*/
+
+    @Test
+    fun `JSON이 데이터클래스 리스트로 변환되는지 확인`() = runTest {
+        val todoData = TodoDataSourceImpl()
+        val jsonList = todoData.getTodo()
+
+        assertNotNull(jsonList)
+        assertTrue(jsonList is List<Todo>)
+        assertEquals(199, jsonList.lastIndex)
     }
 }
