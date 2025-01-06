303315785bd6622ab4ca7084e2ef5743b471b3f8
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-06 18:23:31 +0900

    feat: 연습문제3 JsonList 데이터클래스List변환테스트

--- src/test/kotlin/day14/UserDataSourceImplTest.kt
+++ src/test/kotlin/day14/UserDataSourceImplTest.kt
@@ -1,10 +1,18 @@
 package day14
 
-import org.junit.jupiter.api.Test
+import kotlinx.coroutines.test.runTest
+import kotlin.test.*
 
-import org.junit.jupiter.api.Assertions.*
- class UserDataSourceImplTest {
+class UserDataSourceImplTest {
 
-@Test
- fun getUsers() {}
+    @Test
+    fun `JSON List가 데이터클래스 리스트로 변환되는지 확인`() = runTest {
+        val userData = UserDataSourceImpl()
+        val userList = userData.getUsers()
+
+        assertNotNull(userList)
+        assertTrue(userList is List<User>)
+        assertEquals(9, userList.lastIndex)
+    }
+
 }
