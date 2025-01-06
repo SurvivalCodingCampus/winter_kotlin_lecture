9aafb7c576090bd0df10c97c76c52abd139bc5d9
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-06 20:58:17 +0900

    test: 연습문제4. csv 데이터 변환 테스트

--- src/test/kotlin/day14/StockDataSourceImplTest.kt
+++ src/test/kotlin/day14/StockDataSourceImplTest.kt
@@ -1,10 +1,17 @@
 package day14
 
-import org.junit.jupiter.api.Test
+import kotlinx.coroutines.test.runTest
+import kotlin.test.*
 
-import org.junit.jupiter.api.Assertions.*
- class StockDataSourceImplTest {
+class StockDataSourceImplTest {
 
-@Test
- fun getStockListings() {}
+    @Test
+    fun `CSV 데이터가 데이터클래스 리스트로 변환되는지 확인`() = runTest {
+        val data = StockDataSourceImpl()
+        val stockList = data.getStockListings()
+
+        assertNotNull(stockList)
+        assertTrue(stockList is List<StockListing>)
+        assertEquals(11731, stockList.lastIndex)
+    }
 }
