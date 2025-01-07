010519c2ae57c58367f6932898cade8010a29af3
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-07 21:52:48 +0900

    test: 과제2.PhotoRepository의 getPhotos 테스트

--- src/test/kotlin/day15/repository/PhotoRepositoryImplTest.kt
+++ src/test/kotlin/day15/repository/PhotoRepositoryImplTest.kt
@@ -1,10 +1,16 @@
 package day15.repository
 
-import org.junit.jupiter.api.Test
+import day15.data_source.MockPhotoDatasourceImpl
+import kotlinx.coroutines.test.runTest
+import kotlin.test.*
 
-import org.junit.jupiter.api.Assertions.*
- class PhotoRepositoryImplTest {
+class PhotoRepositoryImplTest {
+    @Test
+    fun `albumId에 일치하는 Json데이터를 가져오는지 확인`() = runTest {
+        val repository = PhotoRepositoryImpl(MockPhotoDatasourceImpl())
+        val photos = repository.getPhotos(1)
 
-@Test
- fun getPhotos() {}
+        assertEquals(50, photos.size)
+    }
+
 }
