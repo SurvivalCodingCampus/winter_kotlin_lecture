c53c0a40a6b136f7c5457b060e842e6dccc28bcb
최현정 <hjchoi.dev@gmail.com>
2025-01-07 20:18:12 +0900

    refactor: Use NetwoorkUtils

--- src/main/kotlin/day15/comment/datasource/RemoteCommentDatasourceImpl.kt
+++ src/main/kotlin/day15/comment/datasource/RemoteCommentDatasourceImpl.kt
@@ -1,7 +1,5 @@
 package org.hyunjung.day15.comment.datasource
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import org.hyunjung.day15.comment.model.Comment
 import org.hyunjung.day15.util.Constants
 import org.hyunjung.day15.util.JsonParser
@@ -10,11 +8,9 @@ import org.hyunjung.day15.util.NetworkUtils
 class RemoteCommentDatasourceImpl : CommentDataSource {
 
     override suspend fun getComments(postId: Int): List<Comment> {
-        return withContext(Dispatchers.IO) {
-            val url = "${Constants.BASE_URL}/comments?postId=$postId"
-            NetworkUtils.executeRequest(url) { response ->
-                JsonParser.parseJson(response)
-            }
+        val url = "${Constants.BASE_URL}/comments?postId=$postId"
+        return NetworkUtils.executeRequest(url) { response ->
+            JsonParser.parseJson(response)
         }
     }
 }
