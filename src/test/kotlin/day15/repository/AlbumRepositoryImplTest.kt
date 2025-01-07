85c80600f70be513e20da7ed9c0c07885facb8c1
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-07 21:55:01 +0900

    test: 과제5.AlbumRepository의 limit null여부에 따른 getAlbums 테스트

--- src/test/kotlin/day15/repository/AlbumRepositoryImplTest.kt
+++ src/test/kotlin/day15/repository/AlbumRepositoryImplTest.kt
@@ -1,10 +1,24 @@
 package day15.repository
 
-import org.junit.jupiter.api.Test
+import day15.data_source.MockAlbumDatasourceImpl
+import kotlinx.coroutines.test.runTest
+import kotlin.test.*
 
-import org.junit.jupiter.api.Assertions.*
- class AlbumRepositoryImplTest {
+class AlbumRepositoryImplTest {
 
-@Test
- fun getAlbums() {}
+    @Test
+    fun `limit이 없을때 모든 Album JSON 데이터를 가져오는지 확인`() = runTest {
+        val repository = AlbumRepositoryImpl(MockAlbumDatasourceImpl())
+        val allAlbum = repository.getAlbums()
+
+        assertEquals(100, allAlbum.size)
+    }
+
+    @Test
+    fun `limit이 있을때 limit수만큼 Album JSON 데이터를 가져오는지 확인`() = runTest {
+        val repository = AlbumRepositoryImpl(MockAlbumDatasourceImpl())
+
+        val albums = repository.getAlbums(2)
+        assertEquals(2, albums.size)
+    }
 }
