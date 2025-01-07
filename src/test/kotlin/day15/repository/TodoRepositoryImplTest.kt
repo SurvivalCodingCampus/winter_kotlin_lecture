81c34826950bbaa4628d9441bb76bf3f07b23569
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-07 21:53:25 +0900

    test: 과제3.TodoRepository의 getTodos,CompletedTodos 테스트

--- src/test/kotlin/day15/repository/TodoRepositoryImplTest.kt
+++ src/test/kotlin/day15/repository/TodoRepositoryImplTest.kt
@@ -1,13 +1,24 @@
 package day15.repository
 
-import org.junit.jupiter.api.Test
+import day15.data_source.MockTodoDatasourceImpl
+import kotlinx.coroutines.test.runTest
+import kotlin.test.*
 
-import org.junit.jupiter.api.Assertions.*
- class TodoRepositoryImplTest {
+class TodoRepositoryImplTest {
 
-@Test
- fun getTodos() {}
+    @Test
+    fun `모든 Todo JSON 데이터를 가져오는지 확인`() = runTest {
+        val repository = TodoRepositoryImpl(MockTodoDatasourceImpl())
+        val todos = repository.getTodos()
 
-@Test
- fun getCompletedTodos() {}
+        assertEquals(200, todos.size)
+    }
+
+    @Test
+    fun `completed true인 JSON 데이터만 가져오는지 확인`() = runTest {
+        val repository = TodoRepositoryImpl(MockTodoDatasourceImpl())
+        val todos = repository.getCompletedTodos()
+
+        assertEquals(90, todos.size)
+    }
 }
