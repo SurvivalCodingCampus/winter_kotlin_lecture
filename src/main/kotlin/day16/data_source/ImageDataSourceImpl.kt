08f388e74107693e789176df4ac31d36296cfc4e
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-09 02:34:40 +0900

    refactor: HttpClient를 매개변수로 받도록 수정

--- src/main/kotlin/day16/data_source/ImageDataSourceImpl.kt
+++ src/main/kotlin/day16/data_source/ImageDataSourceImpl.kt
@@ -1,17 +1,17 @@
 package day16.data_source
 
 import day16.HttpClientFactory
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.File
 
-class ImageDataSourceImpl() : ImageDataSource {
-    private val httpClient = HttpClientFactory.create()
-
+class ImageDataSourceImpl(private val client: HttpClient = HttpClient(CIO)) : ImageDataSource {
     override suspend fun fetchImage(url: String): ByteArray = withContext(Dispatchers.IO) {
-        httpClient.get(url).bodyAsBytes()
+        client.get(url).bodyAsBytes()
     }
 
     override suspend fun saveImage(bytes: ByteArray, path: String) = withContext(Dispatchers.IO) {
