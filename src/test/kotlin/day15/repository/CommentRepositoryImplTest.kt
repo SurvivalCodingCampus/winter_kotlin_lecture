139c222aca77d42e1e0b833d8e9974d82cf137b6
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-08 19:24:28 +0900

    test: 테스트케이스 추가

--- src/test/kotlin/day15/repository/CommentRepositoryImplTest.kt
+++ src/test/kotlin/day15/repository/CommentRepositoryImplTest.kt
@@ -13,4 +13,40 @@ class CommentRepositoryImplTest {
         assertEquals(5, comment.size)
     }
 
+    @Test
+    fun `postId가 범위 밖에 있는 경우 빈 리스트를 가져오는지 확인`() = runTest {
+        val repository = CommentRepositoryImpl(MockCommentDatasourceImpl())
+        val comment1 = repository.getComments(0)
+        val comment2 = repository.getComments(-1)
+
+        assertEquals(0, comment1.size)
+        assertEquals(0, comment2.size)
+    }
+
+    @Test
+    fun `존재하지 않는 postId인 경우 빈 리스트를 반환한다`() = runTest {
+        val repository = CommentRepositoryImpl(MockCommentDatasourceImpl())
+        val comments = repository.getComments(-1)
+        assertTrue(comments.isEmpty())
+    }
+
+    @Test
+    fun `반환된 댓글의 postId가 요청한 postId와 일치하는지 확인`() = runTest {
+        val repository = CommentRepositoryImpl(MockCommentDatasourceImpl())
+        val postId = 1
+        val comments = repository.getComments(postId)
+
+        assertTrue(comments.all { it.postId == postId })
+    }
+
+    @Test
+    fun `데이터 소스에서 예외 발생시 적절히 처리되는지 확인`() = runTest {
+        val mockDataSource = MockCommentDatasourceImpl(shouldThrowException = true)
+        val repository = CommentRepositoryImpl(mockDataSource)
+
+        assertFailsWith<Exception> {
+            repository.getComments(1)
+        }
+    }
+
 }
