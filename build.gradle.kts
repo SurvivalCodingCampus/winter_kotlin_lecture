7f14c9058281d9d6cbc3e1b868a9ca307613a9d0
최현정 <hjchoi.dev@gmail.com>
2025-01-08 16:18:33 +0900

    build: Add Ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+val ktor_version = "3.0.3"
+
 plugins {
     kotlin("jvm") version "2.0.21"
     kotlin("plugin.serialization") version "2.0.21"
@@ -15,6 +17,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
 }
 
 kotlin {
