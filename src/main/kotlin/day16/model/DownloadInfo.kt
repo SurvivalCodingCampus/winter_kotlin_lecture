0a74e503a65572a53e0cac46f235046e769fde15
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-09 02:17:32 +0900

    refactor: 다운로드 정보 프린트 추가

--- src/main/kotlin/day16/model/DownloadInfo.kt
+++ src/main/kotlin/day16/model/DownloadInfo.kt
@@ -5,4 +5,13 @@ data class DownloadInfo(
     val endTime: String,
     val elapsedTime: String,
     val fileSize: String
-)
+) {
+    // 출력 포맷에 맞게 반환하는 메소드
+    fun printDownloadInfo() {
+        println("다운로드 시작")
+        println("다운로드 끝")
+        println("==========")
+        println("소요시간: $elapsedTime")
+        println("용량: $fileSize Bytes")
+    }
+}
