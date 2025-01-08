4297d1c16fe85472440458dfcaab03a27f62e351
junho1124 <wnsghqwe@gmail.com>
2025-01-08 18:43:18 +0900

    chore: update ktor version to 3.0.3 (student/07-최준호)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+val ktor_version: String by project
 val kotestVersion = "5.5.4"
 
 repositories {
@@ -20,6 +21,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
     implementation("com.github.kittinunf.fuel:fuel:3.0.0-alpha04")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
 }
 
 tasks.test {
