c7801fc595e53a0799bc51016a8eac5bda949f88
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-09 03:05:15 +0900

    refactor: 리턴 예외처리 추가

---
+++ src/test/kotlin/day16/repository/TodoRepositoryImplTest.kt
@@ -0,0 +1,13 @@
+package day16.repository
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+ class TodoRepositoryImplTest {
+
+@Test
+ fun getTodos() {}
+
+@Test
+ fun getTodo() {}
+}
