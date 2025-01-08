f5e3a079bb001b14188c0e1e9935cac85dbe6e25
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-09 03:05:15 +0900

    refactor: 리턴 예외처리 추가

--- src/main/kotlin/day16/data_source/ImageDataSourceImpl.kt
+++ src/main/kotlin/day16/data_source/ImageDataSourceImpl.kt
@@ -1,20 +1,35 @@
 package day16.data_source
 
-import day16.HttpClientFactory
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.io.IOException
 import java.io.File
 
 class ImageDataSourceImpl(private val client: HttpClient = HttpClient(CIO)) : ImageDataSource {
-    override suspend fun fetchImage(url: String): ByteArray = withContext(Dispatchers.IO) {
-        client.get(url).bodyAsBytes()
+    override suspend fun fetchImage(url: String): ByteArray {
+        return try {
+            client.get(url).bodyAsBytes()
+        } catch (e: Exception) {
+            ByteArray(0)
+        }
     }
 
-    override suspend fun saveImage(bytes: ByteArray, path: String) = withContext(Dispatchers.IO) {
-        File(path).writeBytes(bytes)
+    override suspend fun saveImage(bytes: ByteArray, path: String) {
+        return try {
+            if (bytes.isNotEmpty()) {
+                File(path).writeBytes(bytes)
+            } else {
+                throw IOException("빈 바이트 배열을 파일에 저장할 수 없습니다.")
+            }
+        } catch (e: IOException) {
+            println("파일 저장 중 오류 발생: ${e.message}")
+            throw e
+        } catch (e: Exception) {
+            println("예상치 못한 오류 발생: ${e.message}")
+            throw e
+        }
     }
+
 }
