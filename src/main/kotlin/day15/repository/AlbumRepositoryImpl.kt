8d560503ccb09533d5a0240f2e9f110849e1f94e
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-08 19:27:15 +0900

    refactor: 데이터슬라이싱 로직을 DataSource로 이동

--- src/main/kotlin/day15/repository/AlbumRepositoryImpl.kt
+++ src/main/kotlin/day15/repository/AlbumRepositoryImpl.kt
@@ -1,13 +1,10 @@
 package day15.repository
 
 import day15.data_source.AlbumDataSource
-import day15.data_source.MockAlbumDatasourceImpl
 import day15.model.Album
-import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.json.Json
 
 class AlbumRepositoryImpl(private val dataSource: AlbumDataSource): AlbumRepository {
     override suspend fun getAlbums(limit: Int?): List<Album> {
-        return if (limit == null) dataSource.getAlbums() else dataSource.getAlbums().take(limit)
+        return dataSource.getAlbums(limit)
     }
 }
