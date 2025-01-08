313d4f0281ac8e1ee8f113862fd8fe1cd5eb9804
최현정 <hjchoi.dev@gmail.com>
2025-01-09 04:43:50 +0900

    feat: Add ApiModule

---
+++ src/main/kotlin/day16/ApiModule.kt
@@ -0,0 +1,20 @@
+package org.hyunjung.day16
+
+import io.ktor.client.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+object ApiModule {
+    fun create(): HttpClient {
+        return HttpClient {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                    coerceInputValues = true
+                })
+            }
+        }
+    }
+}
