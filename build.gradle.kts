a0201bda4f0ca535e2866d444ca56034454f7212
Serim Son <serim711@gmail.com>
2025-01-08 16:51:48 +0900

    chore: add ktor client

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,12 +10,16 @@ repositories {
     mavenCentral()
 }
 
+val ktor_version: String by project
+
 dependencies {
     testImplementation("junit:junit:4.13.2")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.1")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
 }
 
 kotlin {
