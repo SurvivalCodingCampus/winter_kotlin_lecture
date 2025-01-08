a08735f2c3a3abcd56c9695bafc80e74ad6d575f
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-08 11:09:21 +0900

    fix: 데이터소스는 데이터만 가져오게, 필터링은 리포지토리에서 하도록

---
+++ src/test/kotlin/day15/repository/PostRepositoryImplTest.kt
@@ -0,0 +1,13 @@
+package day15.repository
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+ class PostRepositoryImplTest {
+
+@Test
+ fun getPost() {}
+
+@Test
+ fun getPosts() {}
+}
