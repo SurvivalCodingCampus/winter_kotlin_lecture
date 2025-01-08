b9a65fe2ea1d9058d6e7cc04e1fe6fe937a01df8
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-08 19:22:11 +0900

    test: 테스트케이스 추가

--- src/test/kotlin/day15/repository/PhotoRepositoryImplTest.kt
+++ src/test/kotlin/day15/repository/PhotoRepositoryImplTest.kt
@@ -1,7 +1,10 @@
 package day15.repository
 
 import day15.data_source.MockPhotoDatasourceImpl
+import day15.data_source.PhotoDataSource
+import day15.model.Photo
 import kotlinx.coroutines.test.runTest
+import java.io.IOException
 import kotlin.test.*
 
 class PhotoRepositoryImplTest {
@@ -11,6 +14,38 @@ class PhotoRepositoryImplTest {
         val photos = repository.getPhotos(1)
 
         assertEquals(50, photos.size)
+    }
+
+    @Test
+    fun `존재하지 않는 앨범 ID로 조회시 빈 리스트를 반환한다`() = runTest {
+        val repository = PhotoRepositoryImpl(MockPhotoDatasourceImpl())
+        val photos = repository.getPhotos(999)
+        assertTrue(photos.isEmpty())
+    }
+
+    @Test
+    fun `음수 앨범 ID로 조회시 IllegalArgumentException이 발생한다`() = runTest {
+        val repository = PhotoRepositoryImpl(MockPhotoDatasourceImpl())
+
+        assertFailsWith<IllegalArgumentException> {
+            repository.getPhotos(-1)
+        }
+    }
+
+    @Test
+    fun `데이터소스에서 예외 발생시 적절히 처리된다`() = runTest {
+        // 예외를 발생시키는 mock 데이터소스 구현
+        class ErrorMockPhotoDataSource : PhotoDataSource {
+            override suspend fun getPhotos(): List<Photo> {
+                throw IOException("Failed to fetch photos")
+            }
+        }
+
+        val repository = PhotoRepositoryImpl(ErrorMockPhotoDataSource())
+
+        assertFailsWith<IOException> {
+            repository.getPhotos(1)
+        }
     }
 
 }
