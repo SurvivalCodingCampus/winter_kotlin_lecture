72480897451afe0b0bd82e90e2847072f228be01
Serim Son <serim711@gmail.com>
2025-01-09 10:37:28 +0900

    feat: implement image repository

--- src/main/kotlin/day16/data/ImageRepositoryImpl.kt
+++ src/main/kotlin/day16/data/ImageRepositoryImpl.kt
@@ -1,15 +1,34 @@
 package day16.data
 
+import io.ktor.http.*
+import kotlin.io.path.Path
+import kotlin.io.path.absolutePathString
+import kotlin.io.path.exists
+import kotlin.io.path.isDirectory
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
 class ImageRepositoryImpl(private val source: ImageDataSource) : ImageRepository {
     override suspend fun saveImage(url: String, path: String) {
-        TODO("Not yet implemented")
+        val fetched = source.fetchImage(url)
+        source.saveImage(fetched, path)
     }
 
+    @OptIn(ExperimentalUuidApi::class)
     override suspend fun saveImage(urls: List<String>, directory: String) {
-        TODO("Not yet implemented")
+        val destination = Path(directory)
+        require(destination.isDirectory())
+
+        urls.forEach {
+            val ext = Url(it).encodedPath.split('.').last()
+            val fileName = "${Uuid.random()}.${ext}"
+            saveImage(it, destination.resolve(fileName).absolutePathString())
+        }
     }
 
     override suspend fun saveImageIfNotExists(url: String, path: String): Boolean {
-        TODO("Not yet implemented")
+        if (Path(path).exists()) return false
+        saveImage(url, path)
+        return true
     }
 }
--- src/main/kotlin/day16/data/MovieDataSourceImpl.kt
+++ src/main/kotlin/day16/data/MovieDataSourceImpl.kt
@@ -4,13 +4,15 @@ import day16.model.Movie
 import day16.model.RawMovie
 import io.ktor.client.call.*
 import io.ktor.client.request.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class MovieDataSourceImpl : MovieDataSource {
     private val tmdbAPI =
         "https://api.themoviedb.org/3/movie/upcoming?api_key=a64533e7ece6c72731da47c9c8bc691f&language=ko-KR&page=1"
 
-    override suspend fun getUpcomingMovies(): List<Movie> {
+    override suspend fun getUpcomingMovies(): List<Movie> = withContext(Dispatchers.IO) {
         val response = Data.client.get(tmdbAPI).body<RawMovie>()
-        return response.results
+        response.results
     }
 }
