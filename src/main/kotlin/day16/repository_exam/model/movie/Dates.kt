0785c83a5ec335b22d2b4ee23b451cf6ccc32ec6
SibangGyeon <briankim0695@gmail.com>
2025-01-09 11:46:34 +0900

    day16: 네트워크 통신 연습문제 1번 수정

--- src/main/kotlin/day16/repository_exam/model/movie/Dates.kt
+++
@@ -1,9 +0,0 @@
-package org.example.day16.repository_exam.model.movie
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Dates(
-    val maximum: String,
-    val minimum: String,
-)
