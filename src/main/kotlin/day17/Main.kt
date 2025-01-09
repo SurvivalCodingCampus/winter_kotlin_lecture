7872a34e0034a2741fd51f65c762ec969749b731
lcw <skcjddn@gmail.com>
2025-01-10 03:02:41 +0900

    feat: add exam main file

--- src/main/kotlin/day17/Main.kt
+++ src/main/kotlin/day17/Main.kt
@@ -1,7 +1,9 @@
 package org.example.day17
 
 import kotlinx.coroutines.runBlocking
+import org.example.day17.datasource.MockPhotoDataSourceImpl
 import org.example.day17.datasource.StoreDataSourceImpl
+import org.example.day17.repository.PhotoRepositoryImpl
 import org.example.day17.repository.StoreRepositoryImpl
 
 fun main() = runBlocking {
@@ -9,6 +11,14 @@ fun main() = runBlocking {
     val repo = StoreRepositoryImpl(dataSource)
 
     repo.getStores().forEach {
+        //println(it)
+    }
+
+
+    val dataSource2 = MockPhotoDataSourceImpl()
+    val repo2 = PhotoRepositoryImpl(dataSource2)
+
+    repo2.getPhots().forEach {
         println(it)
     }
 }
