0b5b54262c52349030d3c8dc271fb407e39ea617
steve <diasm2@gmail.com>
2025-01-09 10:43:16 +0900

    add : movie model

--- src/main/kotlin/day16/model/Movie.kt
+++ src/main/kotlin/day16/model/Movie.kt
@@ -20,26 +20,26 @@ data class Dates(
 
 @Serializable
 data class Movie(
-    val adult: Boolean,
+    val adult: Boolean?,
     @SerialName("backdrop_path")
-    val backdropPath: String,
+    val backdropPath: String?,
     @SerialName("genre_ids")
-    val genreIds: List<Int>,
+    val genreIds: List<Int>?,
     val id: Int,
     @SerialName("original_language")
-    val originalLanguage: String,
+    val originalLanguage: String?,
     @SerialName("original_title")
-    val originalTitle: String,
+    val originalTitle: String?,
     val overview: String,
-    val popularity: Double,
+    val popularity: Double?,
     @SerialName("poster_path")
-    val posterPath: String,
+    val posterPath: String?,
     @SerialName("release_date")
     val releaseDate: String,
     val title: String,
-    val video: Boolean,
+    val video: Boolean?,
     @SerialName("vote_average")
-    val voteAverage: Double,
+    val voteAverage: Double?,
     @SerialName("vote_count")
-    val voteCount: Int
+    val voteCount: Int?
 )
