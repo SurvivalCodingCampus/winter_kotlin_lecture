075e0a1e3d78e3445e47aefcd77707339277ab10
hodak <jlkim909@gmail.com>
2025-01-09 19:25:02 +0900

    feat: ImageMockEngine 구현

---
+++ src/test/kotlin/day16/data/mock/ImageMockEngine.kt
@@ -0,0 +1,35 @@
+package day16.data.mock
+
+import io.ktor.client.engine.mock.*
+import io.ktor.http.*
+import io.ktor.utils.io.*
+
+val imageMockEngine = MockEngine { request ->
+    when (request.url.toString()) {
+        imgUrlTests[0].url -> {
+            respond(
+                content = ByteReadChannel(imgUrlTests[0].byteArray),
+                status = HttpStatusCode.OK,
+                headers = headersOf(HttpHeaders.ContentType, "image/png")
+            )
+        }
+
+        imgUrlTests[1].url -> {
+            respond(
+                content = ByteReadChannel(imgUrlTests[0].byteArray),
+                status = HttpStatusCode.OK,
+                headers = headersOf(HttpHeaders.ContentType, "image/png")
+            )
+        }
+
+        else -> {
+            respondError(HttpStatusCode.NotFound)
+        }
+    }
+}
+
+val imgUrlTests = listOf(
+    "https://test.com/image1.png" to byteArrayOf(1, 2, 3),
+    "https://test.com/image2.png" to byteArrayOf(3, 4, 5),
+    "https://test.com/image3.png" to byteArrayOf(6, 7, 8),
+).map { ImageUrlTest(it.first, it.second) }
