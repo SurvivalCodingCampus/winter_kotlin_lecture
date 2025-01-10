a20afc8f595a7ca4d5b918efaca0daf1713714d7
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-11 01:21:43 +0900

    refactor: Throwable 상속

--- src/main/kotlin/day18/utils/Result.kt
+++ src/main/kotlin/day18/utils/Result.kt
@@ -1,4 +1,27 @@
 package day18.utils
 
-class Result {
+sealed class Result<out D> {
+    data class Success<out D>(val data: D) : Result<D>()
+    data class Error(val e: PhotoError) : Result<Nothing>()
+}
+
+/*
+enum class NetworkError {
+    NO_INTERNET,
+    UNKNOWN,
+}
+*/
+
+/*sealed class PhotoError : Exception() {
+    object NetworkError : PhotoError()
+    object EmptyQuery : PhotoError()
+    data class ServerError(override val message: String) : PhotoError()
+}*/
+
+sealed class PhotoError : Throwable() {
+    object NetworkError : PhotoError()
+    object EmptyQuery : PhotoError()
+    object EmptyPhoto : PhotoError()
+    data class ServerError(override val message: String) : PhotoError()
+    data class ClientError(override val message: String) : PhotoError()
 }
