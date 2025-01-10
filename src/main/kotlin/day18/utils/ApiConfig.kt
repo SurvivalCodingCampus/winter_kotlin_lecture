13259e00832a98a3b2f4da960bc1f154491e2509
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-11 02:06:33 +0900

    refactor: API키 없을 경우 설정로딩 로직 예외처리 추가

--- src/main/kotlin/day18/utils/ApiConfig.kt
+++ src/main/kotlin/day18/utils/ApiConfig.kt
@@ -8,16 +8,26 @@ data class ApiConfig(
     val pixabay: PixabayConfig
 ) {
     @Serializable
-    data class  PixabayConfig(
+    data class PixabayConfig(
         val apiKey: String
     )
 
     companion object {
-        fun load():String {
-            // application.conf 파일을 읽어오기 위한 설정
-            val config = ConfigFactory.load() // ConfigFactory를 통해 설정 파일을 읽음
-            val pixabayConfig = config.getConfig("pixabay") // pixabay 블록을 읽음
-            return pixabayConfig.getString("apiKey") // apiKey 값을 읽어 반환
+        fun load(): String {
+            return try {
+                val config = ConfigFactory.load() // ConfigFactory를 통해 설정 파일을 읽음
+                if (!config.hasPath("pixabay")) {
+                    throw IllegalStateException("Pixabay 설정을 찾을 수 없습니다.")
+                }
+                val pixabayConfig = config.getConfig("pixabay")// pixabay 블록을 읽음
+                if (!pixabayConfig.hasPath("apiKey")) {
+                    throw IllegalStateException("Pixabay API키가 설정되지 않았습니다.")
+                }
+                return pixabayConfig.getString("apiKey") // apiKey 값을 읽어 반환
+            } catch (e: Exception) {
+                throw IllegalStateException("설정을 로드하는 중 오류가 발생했습니다. ${e.message}")
+            }
         }
     }
+
 }
