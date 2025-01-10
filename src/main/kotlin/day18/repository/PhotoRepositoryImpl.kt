484dbd12ba6ccf8e9fcac560a5083de8c55dd084
Yujin Choe <dev.yujinchoe@gmail.com>
2025-01-11 01:23:17 +0900

    refactor: Result패턴 적용, 테스트 요구사항에 맞는 에러처리

--- src/main/kotlin/day18/repository/PhotoRepositoryImpl.kt
+++ src/main/kotlin/day18/repository/PhotoRepositoryImpl.kt
@@ -3,10 +3,30 @@ package day18.repository
 import day18.data_source.PhotoDataSource
 import day18.mapper.toPhoto
 import day18.model.Photo
+import day18.utils.PhotoError
+import day18.utils.Result
 
 class PhotoRepositoryImpl(private val photoDataSource: PhotoDataSource) : PhotoRepository {
-    override suspend fun getPhotoList(keyword: String): List<Photo> {
-        return photoDataSource.getPhotoList(keyword)
-            .map { it.toPhoto() }
+    override suspend fun getPhotoList(keyword: String): Result<List<Photo>> {
+
+        if (keyword.isEmpty()) {
+            return Result.Error(PhotoError.EmptyQuery)
+        }
+
+        return try {
+            val photos = photoDataSource.getPhotoList(keyword)
+                .map { it.toPhoto() }
+
+            if (photos.isEmpty()) {
+                throw PhotoError.EmptyPhoto
+            }
+
+            Result.Success(photos)
+        } catch (e: Exception) {
+            when (e) {
+                is IllegalStateException -> Result.Error(PhotoError.ServerError(e.message ?: "Unknown Server Error"))
+                else -> Result.Error(PhotoError.NetworkError)
+            }
+        }
     }
 }
