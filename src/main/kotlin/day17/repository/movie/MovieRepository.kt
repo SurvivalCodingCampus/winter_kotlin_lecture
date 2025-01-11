2966d688b2bfa85cd65aafc236b0986badce7dbd
SibangGyeon <briankim0695@gmail.com>
2025-01-11 21:36:24 +0900

    day18 : Result 패턴 수업

--- src/main/kotlin/day17/repository/movie/MovieRepository.kt
+++ src/main/kotlin/day17/repository/movie/MovieRepository.kt
@@ -1,7 +1,8 @@
 package org.example.day17.repository.movie
 
 import org.example.day17.model.movie.Movie
+import org.example.day17.util.movie.Result
 
 interface MovieRepository {
-    suspend fun getMovieInfoList(): List<Movie>
+    suspend fun getMovieInfoList(): Result<List<Movie>>
 }
