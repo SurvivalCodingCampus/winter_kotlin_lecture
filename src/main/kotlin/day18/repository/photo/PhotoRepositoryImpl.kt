7ec6f7e409886c589af0e2e6ca509bdbca1f723f
SibangGyeon <briankim0695@gmail.com>
2025-01-13 21:22:22 +0900

    day18 : Result 패턴 연습문제 1번 수정

--- src/main/kotlin/day18/repository/photo/PhotoRepositoryImpl.kt
+++ src/main/kotlin/day18/repository/photo/PhotoRepositoryImpl.kt
@@ -10,17 +10,15 @@ import org.example.day18.util.photo.Result
 
 class PhotoRepositoryImpl(private val dataSource: PhotoDataSource) : PhotoRepository {
     override suspend fun getPhotos(query: String): Result<List<Photo>, PhotoError> {
-        try {
-            if (query == "") throw IllegalArgumentException()
+        if (query == "") return Result.Error(PhotoError.EmptyQuery)
 
+        try {
             val fixedQuery = query.replace(" ", "+")
             val photos = dataSource.getPhotos(fixedQuery).map { it.toPhoto() }
 
             return Result.Success(photos)
         } catch (e: IOException) {
             return Result.Error(PhotoError.NetWorkError)
-        } catch (e: IllegalArgumentException) {
-            return Result.Error(PhotoError.EmptyQuery)
         } catch (e: ClientRequestException) {
             return Result.Error(PhotoError.ServerError("서버 에러"))
         }
